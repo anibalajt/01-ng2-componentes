56a03b1f0d489bedd21e4aceb52ea35180caebed
Alberto Basalo <albertobasalo@hotmail.com>
2016-06-10 12:10:01 +0200

    docs && css

--- src/app/movimiento/index.ts
+++ src/app/movimiento/index.ts
@@ -1,1 +1,5 @@
-export { MovimientoComponent } from './movimiento.component';
+/** indice, exportador del barril movimiento
+ * se habrá apuntado en system.config*/
+
+/** realmente exporta un solo componente llamado MovimientoComponent */
+export * from './movimiento.component';
