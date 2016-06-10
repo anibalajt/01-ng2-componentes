adb494f8671b00d48dfdb7e4960ec13b8b993628
Alberto Basalo <albertobasalo@hotmail.com>
2016-06-10 12:02:28 +0200

    inid && doc

--- src/app/cash-flow.component.ts
+++ src/app/cash-flow.component.ts
@@ -1,11 +1,20 @@
+/** 1 función para decorar componentes */
 import { Component } from '@angular/core';
 
+
+/** 2 uso de la función decoradora para registrar el componente */
 @Component({
   moduleId: module.id,
   selector: 'cash-flow-app',
   templateUrl: 'cash-flow.component.html',
   styleUrls: ['cash-flow.component.css']
 })
+
+  
+/** 3 definción del componente
+ * es una clase ES6, equivalente a una función controller ES5
+ * sus propiedades y métodos son accesibles a la vista
+ *  */  
 export class CashFlowAppComponent {
   title = 'cash-flow works!';
 }
--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,6 +2,7 @@
 // Different environments can be found in config/environment.{dev|prod}.ts
 // The build system defaults to the dev environment
 
+/** el objeto con valores de configuración */
 export const environment = {
   production: false
 };
