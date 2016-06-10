74ae5a41e090d758d1bbc8e5035e25bb25ad072d
Alberto Basalo <albertobasalo@hotmail.com>
2016-06-10 11:52:31 +0200

    npm install && ng serve

---
+++ src/app/cash-flow.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'cash-flow-app',
+  templateUrl: 'cash-flow.component.html',
+  styleUrls: ['cash-flow.component.css']
+})
+export class CashFlowAppComponent {
+  title = 'cash-flow works!';
+}
